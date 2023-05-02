bc60043790fe96e324b1d60dab21be83d679d6e8
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-05-02 16:47:00 +0200

    slow unit test with range attribute

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public int Value = 123;
+
+		public int GetValue() => Value;
+	}
+}
---
+++ TestProject1/Tests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using ClassLibrary1;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test1([Range(0, 1000, 1)] int value)
+		{
+			var c = new Class1();
+
+			c.Value = value;
+
+			Assert.That(c.GetValue(), Is.EqualTo(value));
+		}
+	}
+}
